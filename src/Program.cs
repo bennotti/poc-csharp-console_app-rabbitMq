fbf6a742272b3c7bf937535a5cdd7cb4ce1dd380
Bruno Bennotti B Moura <brunobennotti@gmail.com>
2021-10-16 20:55:23 -0300

    feat: publish and consumer

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using poc_csharp_console_app_socket_io.Dto;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,7 +11,7 @@ namespace poc_csharp_console_app_socket_io
 {
     class Program
     {
-        public static void exec(MessageInputDto message)
+        public static void execPublish(MessageInputDto message)
         {
             ConnectionFactory _factory = new ConnectionFactory {
                 HostName = "localhost"
@@ -39,6 +40,44 @@ namespace poc_csharp_console_app_socket_io
                     );
                 }
             }
+            Console.WriteLine(" [x] Sent {0}", message.Content);
+        }
+
+        public static void execReceiver()
+        {
+            ConnectionFactory _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "Fila_Console_APP",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var stringfiedMessage = Encoding.UTF8.GetString(body);
+                        var message = JsonConvert.DeserializeObject<MessageInputDto>(stringfiedMessage);
+                        Console.WriteLine(" [x] Received {0}", message.Content);
+                    };
+                    channel.BasicConsume(queue: "Fila_Console_APP",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
         }
 
         static void Main(string[] args)
@@ -48,9 +87,11 @@ namespace poc_csharp_console_app_socket_io
                 Content = "Conteudo da mensagem",
             };
 
-			exec(message);
+			execPublish(message);
 
-            Console.WriteLine("Hello World!");
+            execReceiver();
+
+            Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
         }
     }
